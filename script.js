31baeec4bb9e0ce83bd033a74016f8b2e6ac9603
REYMANBD <abdullahrahat086@gmail.com>
2026-08-19 20:07:19 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-function showMessage() {
-    alert("Welcome to REYMAN.BD");
+
+// REYMAN.BD Cart System
+
+let cart = JSON.parse(localStorage.getItem("reymanCart")) || [];
+
+
+// Add product to cart
+function addToCart(name, price) {
+
+    const product = {
+        name: name,
+        price: price
+    };
+
+    cart.push(product);
+
+    localStorage.setItem("reymanCart", JSON.stringify(cart));
+
+    alert(name + " added to cart!");
+}
+
+
+// Contact button
+function contactUs() {
+
+    alert("Thank you for contacting REYMAN.BD!");
+
 }
